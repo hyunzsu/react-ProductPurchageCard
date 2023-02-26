3c3be80b27457775d07f098aef2aaa3dd2459a9c
hyunzsu <kkm5169@naver.com>
2023-02-26 13:48:16 +0900

    [Add] Counter 버튼 수량 조절 구현

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  const onIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const onDecrease = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className='counter'>
       <div className='counter-button'>
-        <button className='counter-minus'>-</button>
-        <span className='counter-result'>1</span>
-        <button className='counter-plus'>+</button>
+        <button className='counter-minus' onClick={onDecrease}>-</button>
+        <span className='counter-result'>{count}</span>
+        <button className='counter-plus' onClick={onIncrease}>+</button>
       </div>
     </div>
   )
