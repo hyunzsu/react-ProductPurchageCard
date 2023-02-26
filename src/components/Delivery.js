8c60eacfc91104c1a411d37c226829caa54a8cab
hyunzsu <kkm5169@naver.com>
2023-02-26 12:10:11 +0900

    [Add] Delivery 컴포넌트 마크업 완성

---
+++ src/components/Delivery.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Delivery = () => {
+  return (
+    <div className='delivery'>
+      <span>택배배송</span>
+      <span className='line'>|</span>
+      <span>3000원</span>
+      <span className='delivery-text'>(주문시 결제)</span>
+      <div>10개마다 부과</div>
+    </div>
+  )
+}
+
+export default Delivery;
